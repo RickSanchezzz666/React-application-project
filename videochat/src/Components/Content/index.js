f2ffc8183ab246683566e7ebfc937255b81c701b
Maksym67 <maxpasternak54@gmail.com>
2023-02-22 20:48:53 +0200

    temp style added

--- videochat/src/Components/Content/index.js
+++ videochat/src/Components/Content/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import './style.css';
+import MedDocLogo from "./imgs/MedDoc.png";
 import socket from "../../Socket";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init();
 
 function Content () {
     return (
-        <div>
-            main page
+        <div className="mainpage">
+            <img className="unicore-logo" src={MedDocLogo} alt="Not Found" data-aos="fade-down" data-aos-delay="100"/>
+            <a href="room" className="startButton" data-aos="fade-up" data-aos-delay="200">Start meeting<br/>(temporary unavaible)</a>
         </div>
     )
 }
--- videochat/src/Components/NotFoundPage/index.js
+++ videochat/src/Components/NotFoundPage/index.js
@@ -3,8 +3,9 @@ import './style.css';
 
 function NotFound () {
     return (
-        <div>
-            Page not found
+        <div className="NotFoundPage">
+            <h1 className="main-text" data-aos="fade-right" data-aos-delay="100">Error 404</h1>
+            <h2 className="sub-text" data-aos="fade-left" data-aos-delay="200">Page not Found : (</h2>
         </div>
     )
 }
