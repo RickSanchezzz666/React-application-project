a2ca533ae7c1c10f86c4579660948363d62c5bb5
RickSanchezzz666 <lapiyktube@gmail.com>
2023-02-22 16:07:38 +0200

    router

--- videochat/src/App.js
+++ videochat/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Content from './Components/Content';
+import Room from './Components/Room';
+import NotFoundPage from './Components/NotFoundPage';
 
 function App() {
   return (
-    <div className="App">
-      helloilo
-    </div>
+    <BrowserRouter>
+        <Route path='/' element={<Content />}>
+        </Route>
+        <Route path='/room/:id' element={<Room />}>
+        </Route>
+        <Route path='*' element={NotFoundPage}>
+
+        </Route>
+    </BrowserRouter>
   );
 }
 
