c78508e32f35c86f8e69589374d4d1e688fe36af
Maksym67 <maxpasternak54@gmail.com>
2023-02-28 18:26:38 +0200

    ports fix

--- videochat/server.js
+++ videochat/server.js
@@ -14,7 +14,7 @@ require('dotenv').config();
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const PORT = process.env.PORT;
+const PORT = process.env.PORT_BACK;
 app.use(bodyParser.json());
 
 console.log('MONGO_DB_URI:', process.env.MONGO_DB_URI);
