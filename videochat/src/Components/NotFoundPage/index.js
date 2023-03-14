e2c65455de21b0419dcb2620d004de3efa6adfba
Maksym67 <maxpasternak54@gmail.com>
2023-03-14 11:03:06 +0200

    scroll added

--- videochat/src/Components/NotFoundPage/index.js
+++ videochat/src/Components/NotFoundPage/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import './style.css';
+import { useNavigate } from "react-router";
 
 function NotFound () {
+    const history = useNavigate();
+
     return (
         <div className="NotFoundPage">
             <h1 className="main-text" data-aos="fade-right" data-aos-delay="100">Error 404</h1>
             <h2 className="sub-text" data-aos="fade-left" data-aos-delay="200">Page not Found : (</h2>
+            <button onClick={() => {
+                history('/');
+            }} className="back-button">Go to mainpage</button>
         </div>
     )
 }
